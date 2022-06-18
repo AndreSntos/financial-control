5ac49037cf60523201906127226c61f8b27efb6e
Andre Santos <andredasil2002@gmail.com>
2022-06-18 16:16:50 -0300

    🌟feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/header";
+import GlobalStyle from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
